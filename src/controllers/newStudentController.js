4c20d544425b969b13839c312af405f4cefc1443
jiamudza <ajimasbagus12@gmail.com>
2026-04-09 08:23:00 +0700

    change phase to gelombang 3

--- src/controllers/newStudentController.js
+++ src/controllers/newStudentController.js
@@ -59,7 +59,7 @@ export async function addStudent(req, res) {
             bukti_pembayaran: buktiBayarUrl,
             password: hashedPassword,
             status_pembayaran: "Belum Lunas",
-            fase: "Gelombang 2"
+            fase: "Gelombang 3"
         };
 
         // simpan ke supabase
